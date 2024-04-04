0628ce766fd076c1cf4e84b0002d6dc4cf445425
alengusinac <61206891+alengusinac@users.noreply.github.com>
2024-04-04 11:46:33 +0200

    fixed fetching single image

--- main.js
+++ main.js
@@ -2,21 +2,13 @@ import './style.css';
 import * as XLSX from 'xlsx/xlsx.mjs';
 
 const SWISH_URL = 'https://mpc.getswish.net/qrg-swish/api/v1/prefilled';
+const LOCAL_URL = 'http://localhost:3000/swish/generate-qr';
+
+const imageContainer = document.querySelector('#output');
+const linkContainer = document.querySelector('a');
 
 const request = {
   format: 'jpg',
-  payee: {
-    value: '0706931235',
-    editable: false,
-  },
-  amount: {
-    value: 'string',
-    editable: false,
-  },
-  message: {
-    value: 'string',
-    editable: false,
-  },
   size: 300,
 };
 
@@ -45,24 +37,61 @@ document.querySelector('form').addEventListener('submit', (e) => {
         raw: false,
       });
 
-      console.log(result);
       result.forEach(async (row) => {
         console.log(row['nummer']);
-        const response = await fetch(SWISH_URL, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'no-cors': 'true',
-          },
-          body: JSON.stringify({
-            ...request,
-            payee: { value: row['nummer'], editable: false },
-            amount: { value: row['belopp'], editable: false },
-            message: { value: row['meddelande'], editable: false },
-          }),
-        });
-        console.log(response);
+
+        const data = {
+          ...request,
+          payee: { value: row['nummer'], editable: false },
+          amount: { value: row['belopp'], editable: false },
+          message: { value: row['meddelande'], editable: false },
+        };
+
+        const reader = await getSwishQR(data);
       });
     });
+  };
+
+  const getSwishQR = async (data) => {
+    fetch(LOCAL_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        console.log(response);
+        const reader = response.body.getReader();
+        return new ReadableStream({
+          start(controller) {
+            return pump();
+            function pump() {
+              return reader.read().then(({ done, value }) => {
+                // When no more data needs to be consumed, close the stream
+                if (done) {
+                  controller.close();
+                  return;
+                }
+                // Enqueue the next data chunk into our target stream
+                controller.enqueue(value);
+                return pump();
+              });
+            }
+          },
+        });
+      })
+      // Create a new response out of the stream
+      .then((stream) => new Response(stream))
+      // Create an object URL for the response
+      .then((response) => response.blob())
+      .then((blob) => URL.createObjectURL(blob))
+      // Update image
+      .then((url) => {
+        imageContainer.setAttribute('src', url);
+        linkContainer.href = url;
+        linkContainer.setAttribute('download', `swishQR.jpeg`);
+      })
+      .catch((err) => console.error(err));
   };
 });
